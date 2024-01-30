ca2b0daeab892cf698672691430972d1c4aeadc9
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-01-29 18:36:07 -0700

    Fixed data logging twice

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,1 @@
 import {getData} from "./dataHandler.mjs";
-
-getData()
-	.then(() => {
-		// Remove this console.log() for production
-		// console.log('Data loaded successfully');
-	})
-	.catch(error => {
-		console.log('Error loading data', error);
-	});
